58beaa08bf6adae9355b0712b9a03b1ebea8c4d8
Stephen <snetzley@uncc.edu>
2021-04-20 18:50:49 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -4,6 +4,7 @@
 import os                            # os is used to get environment variables IP & PORT
 from flask import Flask              # Flask is the web app that we will customize
 from flask import render_template    # for main.css and index.html
+from flask import request            # for user input
 
 app = Flask(__name__)     # create an app
 
@@ -23,8 +24,9 @@ def get_notes():
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
             }
+    a_user = {'name': 'Stephen', 'email': 'mogli@uncc.edu'}
 
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -32,14 +34,22 @@ def get_note(note_id):
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
-
-    return render_template('note.html', note=notes[int(note_id)])
-
-@app.route('/notes/new')
-def new_note():
     a_user = {'name': 'Stephen', 'email': 'mogli@uncc.edu'}
 
-    return render_template('new.html', user=a_user)
+    return render_template('note.html', note=notes[int(note_id)], user=a_user)
+
+@app.route('/notes/new', methods=['GET', 'POST'])
+def new_note():
+    # create mock user
+    a_user = {'name': 'Stephen', 'email': 'mogli@uncc.edu'}
+
+    # check method used for request
+    print('request method is',request.method)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
+    else:
+        return render_template('new.html', user=a_user)
+
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
