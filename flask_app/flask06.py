673b93a9f5836fe92555122e7d59dd386b6374bb
Stephen <snetzley@uncc.edu>
2021-04-21 23:34:02 -0400

    login registration functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -10,8 +10,8 @@ from database import db              # imports db from database.py
 from models import Note as Note      #
 from models import User as User      #
 from forms import RegisterForm       #
-from bcrypt import hashpw            #
-from bcrypt import gensalt           #
+from forms import LoginForm          #
+import bcrypt                        #
 from flask import session            #
 
 app = Flask(__name__)     # create an app
@@ -124,8 +124,8 @@ def register():
 
     if request.method == 'POST' and form.validate_on_submit():
         # salt and hash password
-        h_password = hashpw(
-            request.form['password'].encode('utf-8'),gensalt())
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'),bcrypt.gensalt())
         # get entered user data
         first_name = request.form['firstname']
         last_name = request.form['lastname']
@@ -134,7 +134,7 @@ def register():
         # add user to database and commit
         db.session.add(new_user)
         db.session.commit()
-        # save tthe user's name to the session
+        # save the user's name to the session
         session['user'] = first_name
         session['user_id'] = new_user.id # access id value from user model of this newly added user
         # show user dashboard view
@@ -143,7 +143,28 @@ def register():
     # something went wrong - display register view
     return render_template('register.html', form=form)
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_form = LoginForm()
+    # validate_on_submit only validates using POST
+    if login_form.validate_on_submit():
+        # we know user exists. We can use one()
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+        # user exists check password entered matches stored password
+        if bcrypt.checkpw(request.form['password'].encode('utf-8'), the_user.password):
+            # password match add user info to session
+            session['user'] = the_user.first_name
+            session['user_id'] = the_user.id
+            # render view
+            return redirect(url_for('get_notes'))
 
+        # password check failed
+        # set error message to alert user
+        login_form.password.errors = ["Incorrect username or password."]
+        return render_template("login.html", form=login_form)
+    else:
+        # form did not validate or GET request
+        return render_template("login.html", form=login_form)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
