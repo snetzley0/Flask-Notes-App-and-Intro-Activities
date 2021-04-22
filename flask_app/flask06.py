c0aa7f201975c1851a5905cdca871ae23c9141d8
Stephen <snetzley@uncc.edu>
2021-04-21 23:27:26 -0400

    added registration functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -9,10 +9,15 @@ from flask import redirect, url_for  # for redirecting back to URLs within funct
 from database import db              # imports db from database.py
 from models import Note as Note      #
 from models import User as User      #
+from forms import RegisterForm       #
+from bcrypt import hashpw            #
+from bcrypt import gensalt           #
+from flask import session            #
 
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'SE3155'
 # Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 # Setup models
@@ -32,11 +37,17 @@ def index():
 
 @app.route('/notes')
 def get_notes():
-    a_user = db.session.query(User).filter_by(email='snetzley@uncc.edu').one()
-    # get notes from database
-    my_notes = db.session.query(Note).all()
+    # # retrieve user from database
+    # check if a user is saved in session
+    if session.get('user'):
+        # get notes from database
+        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
 
-    return render_template('notes.html', notes=my_notes, user=a_user)
+        return render_template('notes.html', notes=my_notes, user=session['user'])
+    else:
+        return redirect(url_for('login'))
+
+
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -106,6 +117,33 @@ def delete_note(note_id):
     db.session.commit()
 
     return redirect(url_for('get_notes'))
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # salt and hash password
+        h_password = hashpw(
+            request.form['password'].encode('utf-8'),gensalt())
+        # get entered user data
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        # create user model
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+        # add user to database and commit
+        db.session.add(new_user)
+        db.session.commit()
+        # save tthe user's name to the session
+        session['user'] = first_name
+        session['user_id'] = new_user.id # access id value from user model of this newly added user
+        # show user dashboard view
+        return redirect(url_for('get_notes'))
+
+    # something went wrong - display register view
+    return render_template('register.html', form=form)
+
+
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
